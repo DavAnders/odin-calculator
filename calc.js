2fe4e124e398dd96e533b8646dc22742927f30a9
DavAnders <123442746+DavAnders@users.noreply.github.com>
2024-04-03 00:07:37 -0500

    Add text input to display

--- calc.js
+++ calc.js
@@ -34,3 +34,45 @@ function operate(operator, num1, num2) {
       return "Invalid operator";
   }
 }
+
+let displayValue = 0;
+
+const updateDisplay = () => {
+  document.getElementById("display-text").value = displayValue;
+};
+
+const numberButtons = document.querySelectorAll(".nums");
+
+numberButtons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    const num = e.target.getAttribute("data-num");
+    if (displayValue === 0) {
+      displayValue = num;
+    } else {
+      displayValue += num;
+    }
+    updateDisplay();
+  });
+});
+
+document.getElementById("display-text").addEventListener("input", (e) => {
+  console.log("Input event fired", e.target.value);
+  const validInput = /^[0-9+\-*/.]*$/;
+  const currentValue = e.target.value;
+
+  if (validInput.test(currentValue)) {
+    displayValue = currentValue;
+  } else {
+    // If the input is invalid, revert to the last valid value and update the input field to reflect this
+    e.target.value = displayValue;
+  }
+});
+
+updateDisplay();
+
+const clearButton = document.querySelector(".c");
+
+clearButton.addEventListener("click", (e) => {
+  displayValue = 0;
+  updateDisplay();
+});
