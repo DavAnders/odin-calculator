e15b7d458385b89ce759418b088c27818f3f9ee2
DavAnders <123442746+DavAnders@users.noreply.github.com>
2024-04-02 22:09:11 -0500

    Add initial files

---
+++ calc.js
@@ -0,0 +1,16 @@
+// Basic math operators
+function add(x, y) {
+  return x + y;
+}
+
+function subtract(x, y) {
+  return x - y;
+}
+
+function multiply(x, y) {
+  return x * y;
+}
+
+function divide(x, y) {
+  return x / y;
+}
