db5a5ac02f07952f2487438c5283af1600093d73
DavAnders <123442746+DavAnders@users.noreply.github.com>
2024-04-02 22:58:24 -0500

    Add buttons and styling setup

--- calc.js
+++ calc.js
@@ -14,3 +14,23 @@ function multiply(x, y) {
 function divide(x, y) {
   return x / y;
 }
+
+// Three parts of calculator operation
+let firstNumber = 0;
+let operator = "";
+let secondNumber = 0;
+
+function operate(operator, num1, num2) {
+  switch (operator) {
+    case "+":
+      return add(num1, num2);
+    case "-":
+      return subtract(num1, num2);
+    case "*":
+      return multiply(num1, num2);
+    case "/":
+      return divide(num1, num2);
+    default:
+      return "Invalid operator";
+  }
+}
